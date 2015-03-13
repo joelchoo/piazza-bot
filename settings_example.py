e1b78c1ca8e1f9510cb0c0cc9931d8a18aa74bda
Joel Choo <joelchoo@gmail.com>
2015-03-13 08:56:31 -0400

    Initial commit

---
+++ settings_example.py
@@ -0,0 +1,24 @@
+#Created by Joel Choo
+#This is the settings file for the Piazza bot!
+
+# For testing purposes. Set to False when not testing.
+DEBUG = True
+
+# Number of posts to check from Piazza
+POSTS_TO_CHECK = 50
+
+# Minimum number of unanswered posts before sending out the emails
+THRESHOLD = 5
+
+# Mailing list for debugging. Most likely your email
+DEBUG_MAILING_LIST = ['your_email@email.com']
+
+# Mailing list for email blast!
+MAILING_LIST = ['people@email.com']
+
+# Your email for Piazza. Sorry I couldn't figure out a nicer way to do this
+PIAZZA_EMAIL = 'your_piazza_email@email.com'
+
+# Your Piazza password (ugh)
+PIAZZA_PASSWORD = 'your_piazza_password'
+
